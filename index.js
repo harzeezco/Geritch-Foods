f50cbd98a4f132938b4b952612299570aa52c4d0
saoban <wakilabdulazeez@gmail.com>
2023-06-06 16:01:29 -0700

    Work on the media query

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const header = document.querySelector('.header');
 const navToggle = document.querySelector('.nav-toggle')
+const body = document.querySelector('html');
 
 navToggle.addEventListener('click', () => {
-   header.classList.toggle('nav-open')
+   header.classList.toggle('nav-open');
+   body.classList.toggle('overflow')
 })
