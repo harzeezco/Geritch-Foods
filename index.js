0d560f35f8877cd57a871f4d9ac759eef6b5ac62
harzeezco <wakilabdulazeez@gmail.com>
2024-04-21 22:28:29 +0100

    fix bug

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const header = document.querySelector('.header');
-const navToggle = document.querySelector('.nav-toggle')
+const navToggle = document.querySelector('.nav-toggle');
 const body = document.querySelector('html');
+const menuList = document.querySelectorAll('.main-nav-list');
 
 navToggle.addEventListener('click', () => {
-   header.classList.toggle('nav-open');
-   body.classList.toggle('overflow')
-})
+	header.classList.toggle('nav-open');
+});
+
+menuList.forEach((menu) => {
+	menu.addEventListener('click', () => {
+		header.classList.remove('nav-open');
+	});
+});
